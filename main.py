930867a4523ef4cde36ab498c9e93de4b4943be9
Kmg926 <115695358+Kmg926@users.noreply.github.com>
2025-12-09 17:08:56 +0900

    Modify 3 results

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ second = input("Enter the second number: ")
 num1 = int(first)
 num2 = int(second)
 
-result = num1 + num2
+sumresult = num1 + num2
+subresult = num1 - num2
+multresult = num1 * num2
 
-print("Sum: " result)
+print("Sum: " sumresult)
+print("Difference: " subresult)
+print("Product: " multresult)
